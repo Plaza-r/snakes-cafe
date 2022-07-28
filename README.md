0ae624cfaac548ac2c85c5fe249b037f5ea06068
RIKI PLAZA <rikiplaza1@gmail.com>
2022-07-28 05:14:31 -0400

    finished lab

--- README.md
+++ README.md
@@ -1,48 +1,6 @@
-print(
-    """
-**************************************
-**    Welcome to the Snakes Cafe!   **
-**    Please see our menu below.    **
-**
-** To quit at any time, type "quit" **
-**************************************
-Appetizers
-----------
-Wings
-Cookies
-Spring Rolls
-Entrees
--------
-Salmon
-Steak
-Meat Tornado
-A Literal Garden
-Desserts
---------
-Ice Cream
-Cake
-Pie
-Drinks
-------
-Coffee
-Tea
-Unicorn Tears
-***********************************
-** What would you like to order? **
-***********************************
-"""
-)
+# Project: Snake-cafe
 
-# initialize empty meal dictionary
+## Lab 01
 
-# start loop here until user enters quit
+### Riki E Plaza
 
-# create a variable to store the user's order
-order = input("> ")
-
-# print the order in some fancy way we're still figuring out
-num_items = 1  # TODO: properly tally items that have been ordered
-report = f"** {num_items} order of {order} have been added to your meal **"
-
-print(report)
-# end loop
--- snakes_cafe.py
+++ snakes_cafe.py
@@ -34,27 +34,44 @@ Unicorn Tears
 )
 
 # initialize empty meal dictionary
-dinner = {}
-
-
-while True:
+# dinner = {}
+#while True:
 # start loop here until user enters quit
 
 #quit = false
 
 # create a variable to store the user's order
-order = input("> ")
-if order == 'quit':
+ #   order = input("> ")
+  #  if order == 'quit':
 
-# print the order in some fancy way we're still figuring out
-if order not in dinner:
-    dinner[order] = 1
-    report = f"** {dinner} order of {order} have been added to your meal **"
-    print =(report)
+        # print the order in some fancy way we're still figuring out
+   # if order not in dinner:
+    #    dinner[order] = 1
+    #report = f"** {dinner} order of {order} have been added to your meal **"
+    #print =(report)
 
-else:
-dinner[order] += 1
-report = f"** {dinner[order] } orders of {order} have been added to your meal **"
+    #else:
+    #dinner[order] += 1
+    #report = f"** {dinner[order] } orders of {order} have been added to your meal **"
 
-print(report)
+      #      print(report)
 # end loop
+
+
+item_count = 0
+order = ""
+
+dinner_order = []
+
+while order != "quit":
+    order = input("> ")
+    dinner_order.append(order)
+    num_items = dinner_order.count(order)
+
+    report1 = f"** {num_items} order of {order} have been added to your meal **"
+    report2 = f"** {num_items} orders of {order} have been added to your meal **"
+
+    if item_count == 1:
+        print(report1)
+    else:
+        print(report2)
